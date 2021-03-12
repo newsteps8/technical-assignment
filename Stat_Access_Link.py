bd6dc747f6d11c51d0fbdcbfd006b713db338bfe
Büşra Gökmen <37721917+newsteps8@users.noreply.github.com>
2021-03-12 18:51:01 +0300

    Add files via upload

---
+++ Stat_Access_Link.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv('stat_access_link.csv')
+# Replace tags and protocol parts with '' to extract pure url
+df['Stats_Access_Link'] = df['Stats_Access_Link'].str.replace('<url>https://','')
+df['Stats_Access_Link'] = df['Stats_Access_Link'].str.replace('</url>','')
+df['Stats_Access_Link'] = df['Stats_Access_Link'].str.replace('<url>http://','')
+df['Stats_Access_Link'] = df['Stats_Access_Link'].str.lower()
+print(df.head(5))
---
+++ data_impute.py
@@ -0,0 +1,23 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+# Load the dataset
+df = pd.read_csv('country_vaccination_stats.csv')
+# Get minimum value of column 'daily_vaccinations
+# Fill the missing data (impute) in daily_vaccinations column per country
+# Group the data by number of rows: once and more than once by country
+df1 = df[df.groupby('country')["country"].transform('count') == 1] # For example, Kuwait
+df2 = df[df.groupby('country')["country"].transform('count') > 1] # For example, Argentina
+df1['daily_vaccinations'] = df1['daily_vaccinations'].fillna(0) # Fill the NaN values with "0" in 'daily_vaccinations' column
+df2["daily_vaccinations"] = df2.groupby("country")["daily_vaccinations"].transform(lambda x: x.fillna(x.min())) # Fill the NaN values with min value in 'daily_vaccinations' column by relevant country
+
+frames = [df2, df1] # list of this frames
+df= pd.concat(frames) # merge df1 and df2 updated dataframes
+
+print(df.head(5))
+
+
+df=df.loc[df['date'] == '1/6/2021']
+Total = df['daily_vaccinations'].sum()
+print(Total)
+
