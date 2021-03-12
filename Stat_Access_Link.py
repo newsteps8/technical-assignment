9660358d95c1883b48338a1c78d75e3564601225
Büşra Gökmen <37721917+newsteps8@users.noreply.github.com>
2021-03-12 18:58:54 +0300

    Update Stat_Access_Link.py

--- Stat_Access_Link.py
+++ Stat_Access_Link.py
@@ -6,5 +6,5 @@ df = pd.read_csv('stat_access_link.csv')
 df['Stats_Access_Link'] = df['Stats_Access_Link'].str.replace('<url>https://','')
 df['Stats_Access_Link'] = df['Stats_Access_Link'].str.replace('</url>','')
 df['Stats_Access_Link'] = df['Stats_Access_Link'].str.replace('<url>http://','')
-df['Stats_Access_Link'] = df['Stats_Access_Link'].str.lower()
+df['Stats_Access_Link'] = df['Stats_Access_Link'].str.lower()# string to lowercase
 print(df.head(5))
